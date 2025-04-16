e81e25ac0c84c5efa859bbb452c3c75112894fc7
Gabriela Serrano <gabrielasc0309@gmail.com>
2025-04-16 09:48:39 -0400

    Implement Destination Dropdown Filter with Controlled Component

--- src/components/DestinationSelector.jsx
+++ src/components/DestinationSelector.jsx
@@ -1,1 +1,26 @@
+// components/DestinationSelector.jsx
+import React from 'react';
 
+const DestinationSelector = ({ tours, selected, setSelected }) => {
+  // Extract unique destination names
+  const uniqueNames = ['All Destinations', ...new Set(tours.map((t) => t.name))];
+
+  return (
+    <div>
+      <label htmlFor="destination">Select Destination:</label>
+      <select
+        id="destination"
+        value={selected}
+        onChange={(e) => setSelected(e.target.value)} // Controlled select
+      >
+        {uniqueNames.map((name, idx) => (
+          <option key={idx} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default DestinationSelector;
